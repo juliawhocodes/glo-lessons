3b94f50ae0db89d8417eec35c3685e39608fdddd
Julia Ivanova <juliawhocodes@gmail.com>
2021-08-02 22:29:11 +0300

    modified with alert

---
+++ script.js
@@ -0,0 +1,10 @@
+let money = 100000;
+let income = 150000;
+let addIncome = 300000;
+let deposit = 30;
+let misson = ('To be rich');
+let period = ('5 years')
+
+alert("Pls confirm you read terms & conditios");
+
+console.log('Never never gonna give you up');
