8eff568b685f7d0dda3adccf25a3325890140775
Julia Ivanova <juliawhocodes@gmail.com>
2021-08-03 14:08:08 +0300

    lesson 2 hw ready

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 let money = 100000;
-let income = 150000;
+let income = 'freelance';
+let addExpenses = 'rent, food, internet, transport, entertainments';
 let addIncome = 300000;
-let deposit = 30;
-let misson = ('To be rich');
-let period = ('5 years')
+let deposit = true;
+let mission = 900000;
+let period = 3
 
-alert("Pls confirm you read terms & conditios");
+//alert("Pls confirm you read terms & conditios");
+//console.log('Never never gonna give you up');
 
-console.log('Never never gonna give you up');
+console.log(money, income, deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяца');
+console.log('Цель заработать ' + mission +' рублей');
+
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+console.log([addExpenses]);
+
+let budgetDay = (450000 / 30);
+console.log(budgetDay);
