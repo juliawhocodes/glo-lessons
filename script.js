85c4deae8257122ee53ada4ff6d9030f5e2e8f91
Julia Ivanova <juliawhocodes@gmail.com>
2021-08-05 12:09:39 +0300

    HW 2 difficult lesson made by easy ideas

---
+++ script.js
@@ -0,0 +1,12 @@
+let number = 266219;
+console.log(number);
+let str = number.toString();
+console.log(str);
+let arr = str.split('');
+console.log([arr]);
+let result1 = arr[0] *  arr[1] *  arr[2] *  arr[3] *  arr[4] *  arr[5];
+    console.log(result1);
+    let result2 = result1 ** 3;
+    console.log(result2);
+    let finalResult = result2.toString();
+    console.log(finalResult.slice(0, 2));
